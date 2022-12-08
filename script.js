a7bcae0e43d0cc39da9dced8576d3ca7797efe7b
it0m0k <mf22009@shibaura-it.ac.jp>
2022-12-08 18:46:48 +0900

    new function

--- script.js
+++ script.js
@@ -15,15 +15,18 @@ var app  = new Vue({
 console.log(app.message)
 
 var app2 = new Vue({
-    el:'app2',
+    el:'#app2',
     data: {
         message:'Hello Vue!'
     }
 })
 
 var app3 = new Vue({
-    el:'app3',
+    el:'#app3',
     data:{
-        colors:['Red','Green','Blue']
+        colors:[
+            {item:'Red'},
+            {item:'Blue'}
+        ]
     }
 })
